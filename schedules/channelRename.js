4cdaeda80d5eed4e1171eecf3dd8379eaf6d7f57
Tolga Ozuygur <tolgaozuygur@gmail.com>
2022-04-01 17:04:32 +0300

    Update channelRename.js
    Fixed a glitch in channel name

--- schedules/channelRename.js
+++ schedules/channelRename.js
@@ -3,7 +3,7 @@ schedule = require("node-schedule");
 module.exports.execute = (client) => {
     if(client.config.auto_change_voice_channel_name === "yes"){
         schedule.scheduleJob(client.helpers.secToCron(client.config.channel_rename_interval), function(){
-            client.channels.cache.get(client.config.voice_channel_id).setName(`┖──${client.localization.commands.water.moisture}: %${client.helpers.arduinoBridge.getMoisture()}`);
+            client.channels.cache.get(client.config.voice_channel_id).setName(`└─${client.localization.commands.water.moisture}: %${client.helpers.arduinoBridge.getMoisture()}`);
         });
     }
 }
