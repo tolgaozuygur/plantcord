852b51f38e70a0a5341cce3b75f27dd57a9c0498
Robotsi <alperenemrekir52@gmail.com>
2022-04-30 22:13:17 +0300

    Update takePicture.js

--- schedules/takePicture.js
+++ schedules/takePicture.js
@@ -2,7 +2,7 @@ const availableOs=["freebsd",'linux','openbsd','sunos','aix'];
 const { scheduleJob, scheduledJobs } = require('node-schedule');
 const NodeWebcam = require('node-webcam');
 const fs = require("fs")
-function takePicture(path) {
+function takePicture(client) {
     var FSWebcam = NodeWebcam.FSWebcam;
     var opts = {
       rotation:client.config.photo_rotation,
@@ -12,27 +12,27 @@ function takePicture(path) {
       output:client.config.photo_ftype
     };
     var webcam = new FSWebcam( opts );
-    webcam.capture(path, function ( err, data ) {} );
+    webcam.capture(client.config.photo_path, function ( err, data ) {} );
     console.log('Took a new picture of the plant!');
   }
 
-function takePictureNonLinux(path) {
+function takePictureNonLinux(client) {
     var opts = {
         quality:80,
         width:client.config.photo_width,
         height:client.config.photo_height,
         output:client.config.photo_ftype
     };
-    NodeWebcam.capture(path, opts, function ( err, data ) {} );
+    NodeWebcam.capture(client.config.photo_path, opts, function ( err, data ) {} );
     console.log('Took a new picture of the plant!');
 }
 
 module.exports.execute = (client) => {
     schedule.scheduleJob(client.helpers.secToCron(client.config.take_photo_interval), function(){
         if(availableOs.includes(process.platform)){
-            takePicture(client.config.photo_path);
+            takePicture(client);
         }else{
-            takePictureNonLinux(client.config.photo_path);
+            takePictureNonLinux(client);
         }
 
         const date = new Date()
