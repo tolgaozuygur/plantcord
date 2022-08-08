b89540588be03bdb73653b0c7c4ea294a7d2b5d5
okunamayanad <baran@okunamayanad.com>
2022-08-08 19:30:07 +0300

    Added Dev Mode

--- helpers/arduinoBridge.js
+++ helpers/arduinoBridge.js
@@ -3,64 +3,90 @@ const localization = require('../localization/' + config.localization_file);
 const { SerialPort } = require('serialport')
 const { ReadlineParser } = require('@serialport/parser-readline')
 var port;
+if (config.dev_mode) {
+	console.log('Running in developer mode')
 
-if (config.arduino_port != "") {
-	port = new SerialPort({ path: config.arduino_port, baudRate: 9600 })
-	port.flush(function (err, results) { });
-	const parser = port.pipe(new ReadlineParser({ delimiter: '\r\n' }))
+	// generating fake variables
+	let moisture = Math.floor(Math.random() * 101)
+	let fan_state = Math.floor(Math.random() * 101)
 
-	moisture = 0;
+	module.exports.getMoisture = () => {
+		return moisture;
+	}
 
-	parser.on('data', data => {
-		//console.log('Arduino:', data);
-		var splitData = data.split("=");
-		if (splitData[0] == "m") {
-			moisture = splitData[1];
+	module.exports.getFanState = () => {
+		return fan_state;
+	}
+
+
+	module.exports.waterThePlant = (pump_time) => {
+		console.log('waterThePlant: ' + pump_time)
+	}
+
+	module.exports.fanspeed = (fan_speed) => {
+		console.log('fanspeed: ' + fan_speed)
+	}
+} else {
+	if (config.arduino_port != "") {
+		port = new SerialPort({ path: config.arduino_port, baudRate: 9600 })
+		port.flush(function (err, results) { });
+		const parser = port.pipe(new ReadlineParser({ delimiter: '\r\n' }))
+
+		moisture = 0;
+
+		parser.on('data', data => {
+			//console.log('Arduino:', data);
+			var splitData = data.split("=");
+			if (splitData[0] == "m") {
+				moisture = splitData[1];
+			}
+		});
+
+	} else {
+		console.error('It looks like you have forgotten to specify the port number on config.json. Please try again with the port number on config.json instead.')
+	}
+
+	module.exports.getMoisture = () => {
+		return moisture;
+	}
+
+	module.exports.getFanState = () => {
+		return fan_state;
+	}
+
+
+	module.exports.waterThePlant = (pump_time) => {
+		if (port != null) {
+			port.write('w' + pump_time + '\n', (err) => {
+				if (err) {
+					return console.log('Error on writing to arduino port: ', err.message);
+				}
+				//console.log('Water plant message sent');
+			});
 		}
-	});
-
-}
-
-module.exports.getMoisture = () => {
-	return moisture;
-}
-
-module.exports.getFanState = () => {
-	return fan_state;
-}
-
-
-module.exports.waterThePlant = (pump_time) => {
-	if (port != null) {
-		port.write('w' + pump_time + '\n', (err) => {
-			if (err) {
-				return console.log('Error on writing to arduino port: ', err.message);
-			}
-			//console.log('Water plant message sent');
-		});
 	}
-}
 
-module.exports.fanspeed = (fan_speed) => {
-	//console.log("thought to arduino");
-	//console.log(fanSpeedMap(fan_speed));
-	if (port != null) {
-		port.write('f' + fanSpeedMap(fan_speed) + '\n', (err) => {
-			if (err) {
-				return console.log('Error on writing to arduino port: ', err.message);
-			}
-		});
+	module.exports.fanspeed = (fan_speed) => {
+		//console.log("thought to arduino");
+		//console.log(fanSpeedMap(fan_speed));
+		if (port != null) {
+			port.write('f' + fanSpeedMap(fan_speed) + '\n', (err) => {
+				if (err) {
+					return console.log('Error on writing to arduino port: ', err.message);
+				}
+			});
+		}
 	}
-}
 
 
-function fanSpeedMap(percentage_fan_speed) {
-	fanSpeedMapped = config.fan_speed_min_value + (percentage_fan_speed / 100 * (config.fan_speed_max_value - config.fan_speed_min_value));
-	if (fanSpeedMapped <= config.fan_speed_min_value) {
-		fanSpeedMapped = 0;
+	function fanSpeedMap(percentage_fan_speed) {
+		fanSpeedMapped = config.fan_speed_min_value + (percentage_fan_speed / 100 * (config.fan_speed_max_value - config.fan_speed_min_value));
+		if (fanSpeedMapped <= config.fan_speed_min_value) {
+			fanSpeedMapped = 0;
+		}
+		if (fanSpeedMapped >= config.fan_speed_max_value - 10) {
+			fanSpeedMapped = config.fan_speed_max_value;
+		}
+		return fanSpeedMapped;
 	}
-	if (fanSpeedMapped >= config.fan_speed_max_value - 10) {
-		fanSpeedMapped = config.fan_speed_max_value;
-	}
-	return fanSpeedMapped;
 }
